24e1a8b3a2b105461bf89dc4f737206f19723e70
Leonard <leonard@uplg.xyz>
2026-03-02 00:46:09 +0100

    fix: avoid render-blocking requests

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  build: {
+    inlineStylesheets: "always",
+  },
   integrations: [react()]
 });
